09154afe913d204fac3e529de8390530546b57ed
Ankit <ankit@ankit-macbookair.local>
2022-10-11 22:05:36 +0530

    code formatting

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,4 @@
 import { Request, Response, NextFunction, Router } from "express";
-import validateUser from "../utils/validation/userSchema";
-import User from "../models/userModel";
 import { createNewUser, getAllUsers } from "../controllers/userController";
 const router: Router = Router();
 
